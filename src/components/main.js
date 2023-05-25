ea7318a2f22300305a3652da17f17a66b30782f3
“joaquincapdevila” <joaquincapdevila4@gmail.com>
2023-05-25 12:41:32 -0300

    creacion de carpeta components

---
+++ src/components/main.js
@@ -0,0 +1,9 @@
+function Main(promps) {
+    return (
+        <div className="container">
+            <h1>esta es una pagina react de prueba para probar git</h1>
+        </div>
+    )
+}
+
+export default Main;
