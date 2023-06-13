01cbae9437890666c3dd5b4f092688191dcd1426
“joaquincapdevila” <joaquincapdevila4@gmail.com>
2023-06-13 20:13:36 -0300

    cambio de estilos

--- src/components/main.js
+++
@@ -1,9 +0,0 @@
-function Main(promps) {
-    return (
-        <div className="container">
-            <h1>esta es una pagina react de prueba para probar git</h1>
-        </div>
-    )
-}
-
-export default Main;
