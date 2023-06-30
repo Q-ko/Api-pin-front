36e6efe7a56b5515cbef44960d26e8f84dba62ba
“joaquincapdevila” <joaquincapdevila4@gmail.com>
2023-06-30 14:00:34 -0300

    comienzo de front-end

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import "./App.css";
-import Header from "./components/sections/header";
-
-import PagePrincipal from "./components/sections/main/pages/Home";
-import Productos from "./components/sections/main/pages/Productos";
-import Nosotros from "./components/sections/main/pages/Nosotros";
-import { BrowserRouter, Route, Link, Routes, Outlet } from "react-router-dom";
-import AlgoMas from "./components/sections/main/pages/AlgoMas";
-import Contacto from "./components/sections/main/pages/Contacto";
+import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import Nav from "./components/sections/header/components/Nav";
 
 function App() {
-  const [isHome, setIsHome] = useState(true);
-
-  /*  let imageBack = "";
-   if (isHome) {
-     imageBack = 'fondo-de-img';
-   }; */
-
-  console.log(isHome);
-
   return (
-    <div className={isHome ? "fondo-de-img height" : ""}>
+    <div>
       <header>
-        <Nav setIsHome={setIsHome} />
+        <Nav />
       </header>
       <main className="main">
         <Outlet />
--- src/components/sections/main/pages/ErrorNotFound.js
+++ src/components/sections/main/pages/ErrorNotFound.js
@@ -1,17 +1,13 @@
-import { useRouteError, Link } from "react-router-dom"
-
+import { useRouteError, Link } from "react-router-dom";
 
 export const NotFound = () => {
+  const error = useRouteError();
+  console.log(error);
 
-    const error = useRouteError();
-    console.log(error);
-
-
-    return (
-        <div>
-            <h1>Error 404</h1>
-            <Link to="/">Volver al Home</Link>
-        </div>
-    );
-
+  return (
+    <div>
+      <h1>La pagina no existe</h1>
+      <Link to="/">Volver al Home</Link>
+    </div>
+  );
 };
