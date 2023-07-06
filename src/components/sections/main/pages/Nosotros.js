79843c937445de3e0358a74c4e20269d73c27516
“joaquincapdevila” <joaquincapdevila4@gmail.com>
2023-07-06 20:58:31 -0300

    cambios en nosotros

--- src/components/sections/main/pages/Nosotros.js
+++ src/components/sections/main/pages/Nosotros.js
@@ -1,9 +1,48 @@
 function Nosotros(promps) {
   return (
-    <div className="img-nos h-100">
-      <div className="d-flex flex-column justify-content-center w-100 h-100 mt-navbar">
-        <div className="row d-flex justify-content-center w-100 h-100">
-          <div className="col-12 col-lg-6 d-flex align-items-center w-100 h-100"></div>
+    <div className="d-flex flex-column position-absolute w-100 h-100 img-nos ">
+      <div className="container d-flex flex-column justify-content-center h-100">
+        <div className="row d-flex flex-column align-items-start justify-content-center pt-lg-5 pt-md-4 pt-sm-3  h-25">
+          <div className="col-12 col-lg-6 h-100">
+            <h2 className="h2-title text-info h-100">
+              AQUI LA PAGINA MAS BUENA DEL MUNDO
+            </h2>
+          </div>
+        </div>
+        <div className="row d-flex flex-column align-items-start justify-content-center my-2 h-75">
+          <div className="col-12 h-100">
+            <p className=" text-light h-100">
+              <strong>
+                En Chilling Real State, llevamos más de 90 años creando hogares
+                de lujo y ensueño en todo el mundo. Fundada por Chilling y
+                continuada por su hijo, nuestra empresa familiar ha transmitido
+                un legado de excelencia y confianza a lo largo de los años.
+                Desde nuestros humildes comienzos, nos enorgullece haber vendido
+                casas por todo el mundo, ayudando a las familias a convertir sus
+                sueños en realidad y encontrar tranquilidad en sus hogares.
+                Nuestro compromiso con la calidad y la satisfacción del cliente
+                es inquebrantable. <br />
+                <br />
+                En Chilling Real State, contamos con un equipo de profesionales
+                apasionados y experimentados dedicados a brindar un servicio
+                personalizado. Valoramos la confianza de nuestros clientes y nos
+                esforzamos por superar sus expectativas en cada paso del
+                proceso. Ya sea que busque una casa de lujo exclusiva o una
+                propiedad única en el extranjero, en Chilling Real State
+                ofrecemos una selección incomparable de propiedades que encarnan
+                el lujo, el diseño impecable y la elegancia atemporal.
+                Trabajamos de cerca con nuestros clientes para hacer realidad
+                sus sueños y ayudarles a vivir con tranquilidad y confianza en
+                un hogar que refleje su estilo de vida y aspiraciones. <br />
+                <br />
+                Nuestra misión es clara: forjar hogares de ensueño que
+                trasciendan las expectativas y se conviertan en un legado
+                duradero para las familias de hoy y las generaciones venideras.
+                Bienvenido a Chilling Real State, donde convertimos sueños en
+                realidad y hogares en legados.
+              </strong>
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -11,41 +50,3 @@ function Nosotros(promps) {
 }
 
 export default Nosotros;
-
-// <div className="row">
-//               <div className="col-12 col-lg-8 d-flex mb-3">
-//                 <h2 className="h2-title">
-//                   Forjando hogares de ensueño con confianza y legado
-//                 </h2>
-//               </div>
-//               <div className="col-12 mt-3">
-//                 <p className="parrafo-comun">
-//                   En Chilling Real State, llevamos más de 90 años creando
-//                   hogares de lujo y ensueño en todo el mundo. Fundada por
-//                   Chilling y continuada por su hijo, nuestra empresa familiar ha
-//                   transmitido un legado de excelencia y confianza a lo largo de
-//                   los años. Desde nuestros humildes comienzos, nos enorgullece
-//                   haber vendido casas por todo el mundo, ayudando a las familias
-//                   a convertir sus sueños en realidad y encontrar tranquilidad en
-//                   sus hogares. Nuestro compromiso con la calidad y la
-//                   satisfacción del cliente es inquebrantable. <br />
-//                   En Chilling Real State, contamos con un equipo de
-//                   profesionales apasionados y experimentados dedicados a brindar
-//                   un servicio personalizado. Valoramos la confianza de nuestros
-//                   clientes y nos esforzamos por superar sus expectativas en cada
-//                   paso del proceso. Ya sea que busque una casa de lujo exclusiva
-//                   o una propiedad única en el extranjero, en Chilling Real State
-//                   ofrecemos una selección incomparable de propiedades que
-//                   encarnan el lujo, el diseño impecable y la elegancia
-//                   atemporal. Trabajamos de cerca con nuestros clientes para
-//                   hacer realidad sus sueños y ayudarles a vivir con tranquilidad
-//                   y confianza en un hogar que refleje su estilo de vida y
-//                   aspiraciones. <br />
-//                   Nuestra misión es clara: forjar hogares de ensueño que
-//                   trasciendan las expectativas y se conviertan en un legado
-//                   duradero para las familias de hoy y las generaciones
-//                   venideras. Bienvenido a Chilling Real State, donde convertimos
-//                   sueños en realidad y hogares en legados.
-//                 </p>
-//               </div>
-//             </div>
