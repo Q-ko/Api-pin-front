6cd1df7deac5df66e498ceb4261d37042c5fd232
“joaquincapdevila” <joaquincapdevila4@gmail.com>
2023-07-12 12:46:56 -0300

    cambios definitivo de funcionamiento c-s

--- src/components/sections/main/pages/Servicios.js
+++ src/components/sections/main/pages/Servicios.js
@@ -6,24 +6,61 @@ function Servicios(promps) {
   return (
     <div className="container d-flex align-items-center justify-content-center h-100 servicios">
       <div className="h-75 w-75 bg-img">
-        <div className="d-flex align-items-end flex-column justify-content-end h-100">
-          <article class="mt-auto p-2">
-            <div class="">
-              <div class="d-flex mb-3">
-                <div class="ps-3">
-                  <h6 class="m-0 h6">nombre de persona</h6>
-                  <p class="m-0 h6">Lorem, ipsum.</p>
+        <div className="container h-100">
+          <div className="row height-mitad"></div>
+          <div className="row height-mitad2 d-flex  justify-content-end">
+            <div className="col-12 col-lg-6 col-md-8 col-sm-10 bg-primary">
+              <div
+                id="carouselServis"
+                class="carousel slide h-100 d-flex align-items-center justify-content-center"
+              >
+                <div class="carousel-inner ">
+                  <div class="carousel-item active">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-8">
+                          <h5 className="h5">TITULO DEL ACROUSEL</h5>
+                        </div>
+                        <div className="col-12">
+                          <p className="h6">
+                            texto latgo pero muy latgo del carousel con
+                            informacion detallada
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-8">
+                          <h5 className="h5">TITULO DEL ACROUSEL</h5>
+                        </div>
+                        <div className="col-12">
+                          <p className="h6">
+                            texto latgo pero muy latgo del carousel con
+                            informacion detallada
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p class="text-light h6">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  vero sequi laborum eaque ab minima rerum beatae, nam
-                  perferendis, quos fugiat saepe qui, iure itaque?
-                </p>
+                <button
+                  class="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselServis"
+                  data-bs-slide="prev"
+                ></button>
+                <button
+                  class="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselServis"
+                  data-bs-slide="next"
+                ></button>
               </div>
             </div>
-          </article>
+          </div>
         </div>
       </div>
     </div>
